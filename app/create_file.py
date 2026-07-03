6fc934ce3b021bba539a527a9f5317248a60abee
Анастасия Логвин <anastasialogvin@MacBook-Air-Anastasia-4.local>
2026-07-03 20:55:48 +0300

    test-passing solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,29 +1,32 @@
 import os
-import sys
 import datetime
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", dest="file_name", required=False)
+parser.add_argument("-d", dest="dir_path", nargs="+", required=False)
+args = parser.parse_args()
 
 
 def create_file_in_directory() -> None:
-    if "-d" in sys.argv and "-f" in sys.argv:
-        path = (sys.argv[sys.argv.index("-d") + 1:sys.argv.index("-f")])
-        create_directory(path)
-        os.chdir(os.path.join(*path))
-        create_file()
-    elif "-d" in sys.argv:
-        path = sys.argv[sys.argv.index("-d") + 1:]
-        create_directory(path)
-    elif "-f" in sys.argv:
-        create_file()
+    if args.dir_path and args.file_name:
+        create_directory(args.dir_path)
+        create_file(args.dir_path)
+    elif args.dir_path:
+        create_directory(args.dir_path)
+    elif args.file_name:
+        create_file([])
 
 
 def create_directory(path: list) -> None:
-    os.makedirs(os.path.join(*path))
+    os.makedirs(os.path.join(*path), exist_ok=True)
 
 
-def create_file() -> None:
-    path = os.path.join(os.getcwd(), sys.argv[sys.argv.index("-f") + 1])
-    with open(path, "a+") as f:
-        if os.path.getsize(path) > 0:
+def create_file(path: list) -> None:
+    file_path = os.path.join(*path, args.file_name)
+    with open(file_path, "a") as f:
+        if os.path.getsize(file_path) > 0:
             f.write("\n\n")
         f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         line_num = 0
@@ -35,5 +38,4 @@ def create_file() -> None:
             f.write(f"\n{str(line_num)} {line}")
 
 
-if __name__ == "__main__":
-    create_file_in_directory()
+create_file_in_directory()
